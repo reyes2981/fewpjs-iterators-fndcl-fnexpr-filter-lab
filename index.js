b612bcd580ddb5d9eadfb94d0ece84f00ae148be
noe <noe.reyes92@yahoo.com>
2021-08-01 21:52:41 -0500

    commit tree 45da05343660ad5f84bce3ac805a34c337a40589

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(source, string) {
+    return source.filter( possMatch =>
+        possMatch.toLowerCase() === string.toLowerCase()
+        )
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter( record => record.name === soughtName)
+  }
